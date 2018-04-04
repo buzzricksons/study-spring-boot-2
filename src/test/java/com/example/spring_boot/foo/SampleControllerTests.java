06fd448edd92d73f9fb21c274dea459df4f0f93a
HyungCheol Kim <hcgoon@gmail.com>
2018-04-04 18:40:43 +0900

    add selenide test

--- src/test/java/com/example/spring_boot/foo/SampleControllerTests.java
+++ src/test/java/com/example/spring_boot/foo/SampleControllerTests.java
@@ -1,9 +1,12 @@
 package com.example.spring_boot.foo;
 
 import org.junit.Test;
+import static com.codeborne.selenide.Selenide.open;
 
 public class SampleControllerTests {
     @Test
     public void testSampleController() {
+        open("localhost:11080");
+
     }
 }
